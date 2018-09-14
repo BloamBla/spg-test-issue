591cbe2645af25ee97e0f8e02df1b3cb5441edb6
Stanislav Khromazhenkov <staslife1994@gmail.com>
2018-09-14 10:19:55 +0300

    first edition(test)
    - was do page layout;
    - connected data.json file(controller will add photos and descriptions for movies in html);
    -added video through HTML5 Video Tag.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,28 @@ import '../styles/main.scss';
 import './modules/main-module.js';
 
 angular.module('App').controller('mainController',
-    function ($scope) {
-        $scope.data = require('./data.json');
-    });
+    function ($scope, $http) {
+
+        $scope.choosenMovie = [];
+        $scope.img_links = [];
+        $scope.movies = [];
+
+        $http({method: 'GET', url: './data.json'}).
+            then(function success(response) {
+                $scope.movies = response.data;
+                $scope.choosenMovie = $scope.movies[0];
+                for (let i = 0; i < $scope.movies.length; i++) {
+                    $scope.img_links = $scope.img_links.concat($scope.movies[i].preview);
+                }
+        });
+
+        $scope.choose = function (index) {
+            $scope.choosenMovie = $scope.movies[index];
+        };
+
+        $scope.setStyles = function (index) {
+            document.getElementsByClassName('items-image-div-wrap')[index].style.background =
+                'url(./src/img/' + $scope.movies[index].preview + ')' + 'no-repeat center';
+            document.getElementsByClassName('items-image-div-wrap')[index].style.backgroundSize = 'contain';
+        };
+});
